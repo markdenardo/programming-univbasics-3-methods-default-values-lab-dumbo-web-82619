a3ef39a6087f63dc366df4744934f5c1f55bdd65
Mark DeNardo <mark.denardo@gmail.com>
2019-08-12 18:54:33 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -9,5 +9,5 @@
 
 def meal_choice (veg1,veg2,protein="meat")
 your_order =  "A plate of #{protein} with #{veg1} and #{veg2}."
-puts "What a nutritious meal!"+your_order
+puts "What a nutritious meal!"+#{your_order}
 end
