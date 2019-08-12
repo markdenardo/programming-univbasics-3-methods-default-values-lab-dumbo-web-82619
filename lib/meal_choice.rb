b13f1cf2a56633710801a30fa6d6ae63f7f4afef
Mark DeNardo <mark.denardo@gmail.com>
2019-08-12 18:52:58 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -8,7 +8,6 @@
  # #meal_choice should puts "What a nutritious meal!" and your order to the console
 
 def meal_choice (veg1,veg2,protein="meat")
-
 your_order =  "A plate of #{protein} with #{veg1} and #{veg2}."
 puts "What a nutritious meal! +#{your_order}"
 end
