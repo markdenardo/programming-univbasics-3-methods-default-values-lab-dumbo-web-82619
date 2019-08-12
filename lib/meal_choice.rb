a96fea34dd6c54ec490adba72176f95a60e1382e
Mark DeNardo <mark.denardo@gmail.com>
2019-08-12 18:20:41 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,8 +1,8 @@
 # Your code here
 def meal_choice (veg1, veg2, protein="meat")
-  meal_choice = "A plate of #{protein} with #{veg1} and #{veg2}."
+  your_order = "A plate of #{protein} with #{veg1} and #{veg2}."
 puts "What a delicious meal!"
-return meal_choice
+
 end
 
-
+ # #meal_choice should puts "What a nutritious meal!" and your order to the console
